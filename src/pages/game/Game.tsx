5476de01c0c41c50bb963e3fed3ea47025f07789
Alexander Shchegol <zelenzoom@gmail.com>
2021-02-06 02:34:29 +0300

    #64_spawn / * динамическое отображение статистики на панели интерфейса

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ApplicationState } from '@store/types';
 import Interface from '@pages/game/interface/Interface';
+import { EngineBus, SPRITE_DESTROYED } from '@engine/EngineBus';
+import BasicTank from '@engine/sprites/enemies/BasicTank';
+import Bullet from '@engine/sprites/Bullet';
 
 export default function Game() {
   const login = useSelector((state: ApplicationState) => state.auth.user.login);
+  const [gameParams, setGameParams] = useState({
+    player: {
+      name: login || 'PLAYER 1',
+      lives: 2,
+      score: 0,
+      kills: 0,
+    },
+    enemies: 20,
+  });
+
+  const palyerShot = (ctx: BasicTank | Bullet) => {
+    if (ctx instanceof BasicTank) {
+      setGameParams((oldParams) => ({
+        ...oldParams,
+        player: {
+          ...oldParams.player,
+          kills: oldParams.player.kills + 1,
+          score: oldParams.player.score + 300,
+        },
+        enemies: oldParams.enemies - 1,
+      }));
+    }
+  };
+
+  useEffect(() => {
+    EngineBus.on(SPRITE_DESTROYED, palyerShot);
+  }, []);
 
   return (
     <div className="container">
@@ -42,7 +72,8 @@ export default function Game() {
       </div>
 
       <Interface
-        player={{ name: login }}
+        enemies={gameParams.enemies}
+        player={gameParams.player}
       />
     </div>
   );
