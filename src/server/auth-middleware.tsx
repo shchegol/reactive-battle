62e9e86946b05b5e1fbd44702e9f5b79598b4ba9
zemer <dmitry.sokun@element.global>
2021-03-01 11:42:20 +0300

    Эксперименты

--- src/server/auth-middleware.tsx
+++ src/server/auth-middleware.tsx
@@ -3,7 +3,26 @@ import yandexAxios from '@root/utils/yandexApiRequest';
 // import cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 
-export const auth = async (req: Request, _res: Response, next: NextFunction) => {
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const { cookie } = req.headers;
+  const options = {
+    headers: {
+      cookie,
+    },
+  };
+
+  console.log(options);
+
+  yandexAxios.get(`${AuthAPI.prefix}/user`, options)
+    .then((data) => {
+      res.locals.user = data;
+      next();
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(401).send({ reason: 'Cookie is not valid' });
+    });
+
   console.log(req.signedCookies);
 
   // const values = cookieParser.JSONCookies(req.cookies);
@@ -22,20 +41,20 @@ export const auth = async (req: Request, _res: Response, next: NextFunction) => 
   //   .map(([key, value]) => `${key}=${value}`)
   //   .join(';');
 
-  try {
-    const authRes = await yandexAxios.get(`${AuthAPI.prefix}/user`, {
-      // headers: { Cookie: cookies },
-    });
+  // try {
+  //   const authRes = await yandexAxios.get(`${AuthAPI.prefix}/user`, {
+  //     // headers: { Cookie: cookies },
+  //   });
 
-    console.log(authRes.status, authRes);
+  //   console.log(authRes.status, authRes);
 
-    //   authRes.status ===
+  //   //   authRes.status ===
 
-    //   req.state.user = data;
-  } catch (err) {
-    console.error(err);
-    // ctx.state.user = null;
-  }
+  //   //   req.state.user = data;
+  // } catch (err) {
+  //   console.error(err);
+  //   // ctx.state.user = null;
+  // }
 
-  next();
+  // next();
 };
