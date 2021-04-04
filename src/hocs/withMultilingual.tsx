bf4f76ddc1a3098d01e38abb554664774edd40ce
Alexander Shchegol <zelenzoom@gmail.com>
2021-04-05 00:06:16 +0300

    #159 Попытка определения языка

--- src/hocs/withMultilingual.tsx
+++ src/hocs/withMultilingual.tsx
@@ -2,17 +2,44 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 import messages_ru from '@root/lang/ru.json';
 import messages_en from '@root/lang/en.json';
+// import { IS_SERVER } from '@root/constants';
+
+enum Locales {
+  ru = 'ru',
+  en = 'en',
+}
 
 export const withMultilingual = (WrappedComponent: React.FC) => () => {
   const messages = {
-    ru: messages_ru,
     en: messages_en,
+    ru: messages_ru,
   };
+  // let locale: string | null = null;
+  // let cachedLocale: string | null = null;
+  const currLocale = 'ru';
+
+  // if (!IS_SERVER) {
+  //   // eslint-disable-next-line prefer-destructuring
+  //   locale = navigator.language.split(/[-_]/)[0];
+  //   cachedLocale = localStorage.getItem('lang');
+  // }
+  //
+  // if (
+  //   !cachedLocale
+  //     && typeof locale === 'string'
+  //     && !!messages[locale as Locales]
+  // ) {
+  //   localStorage.setItem('lang', locale);
+  // }
+  //
+  // if (cachedLocale) {
+  //   currLocale = cachedLocale;
+  // }
 
   return (
     <IntlProvider
-      locale="ru"
-      messages={messages.ru}
+      locale={currLocale}
+      messages={messages[currLocale as Locales]}
     >
       <WrappedComponent />
     </IntlProvider>
